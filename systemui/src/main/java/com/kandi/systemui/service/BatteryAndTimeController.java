1630299f74920f13231df756cf5e8affb8ac948b
HHFly <457588502@qq.com>
2018-09-28 15:00:48 +0800

    1修复状态栏时间显示

--- systemui/src/main/java/com/kandi/systemui/service/BatteryAndTimeController.java
+++ systemui/src/main/java/com/kandi/systemui/service/BatteryAndTimeController.java
@@ -1,10 +1,12 @@
 package com.kandi.systemui.service;
 
 import android.os.Handler;
+import android.os.Message;
 import android.os.RemoteException;
 import android.text.format.DateFormat;
 import android.util.Log;
 
+import com.kandi.systemui.base.App;
 import com.kandi.systemui.driver.DriverServiceManger;
 import com.kandi.systemui.driver.EcocEnergyInfoDriver;
 import com.kandi.systemui.util.TimeUtils;
@@ -14,11 +16,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class BatteryAndTimeController {
     KandiSystemUiService mService;
     Timer mTime;
     TimerTask mTimerTask;
+    public boolean timeFlag =true;
+
     public BatteryAndTimeController(KandiSystemUiService service) {
         mService = service;
         addBatteryAndTimeObserve();
@@ -61,13 +67,17 @@ public class BatteryAndTimeController {
         public void handleMessage(android.os.Message msg) {
             if (msg.what == 0) {
                 refreshPannel();
-                refreshTime();
+//                refreshTime();
                 WifiController.update(mService);
+            }
+            if(msg.what==1){
+                mService.setCurrentTime(DateFormat.is24HourFormat( mService.getApplicationContext())?TimeUtils.getHour(): TimeUtils.getHour_Min12());
             }
         };
     };
     
     public void addBatteryAndTimeObserve(){
+        init_time();
         mTime = new Timer();
         mTimerTask = new TimerTask() {
             @Override
@@ -80,5 +90,25 @@ public class BatteryAndTimeController {
     }
     public void removeBatteryAndTimeObserve(){
         mTime.cancel();
+    }
+
+    //日期 时间模块初始化
+    private void init_time() {
+        ExecutorService timePool = Executors.newSingleThreadExecutor();  //采用线程池单一线程方式，防止被杀死
+        timePool.execute(new Runnable() {
+            @Override
+            public void run() {
+
+                while (timeFlag) {
+                    try {
+                        //延时一秒作用
+                        mHandler.sendEmptyMessage(1);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
