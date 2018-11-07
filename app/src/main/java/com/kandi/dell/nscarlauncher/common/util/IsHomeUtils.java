18c6d437a152aee764b42cbd93765e91302bf25a
HHFly <457588502@qq.com>
2018-11-07 15:46:07 +0800

    update

--- app/src/main/java/com/kandi/dell/nscarlauncher/common/util/IsHomeUtils.java
+++ app/src/main/java/com/kandi/dell/nscarlauncher/common/util/IsHomeUtils.java
@@ -2,15 +2,40 @@ package com.kandi.dell.nscarlauncher.common.util;
 
 import android.app.ActivityManager;
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.Log;
 
 import java.util.List;
 
+import static android.content.Context.ACTIVITY_SERVICE;
+
 public class IsHomeUtils {
     public static boolean isHome(Context context) {
-        ActivityManager mActivityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager mActivityManager = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
         List<ActivityManager.RunningTaskInfo> rti = mActivityManager.getRunningTasks(1);
         Log.d("SystemUI",rti.get(0).topActivity.getPackageName());
         return "com.kandi.nscarlauncher".equals(rti.get(0).topActivity.getPackageName());
+    }
+
+    /**
+     * 判断某个界面是否在前台
+     *
+     * @param context   Context
+     * @param className 界面的类名
+     * @return 是否在前台显示
+     */
+    public static boolean isForeground(Context context, String className) {
+        if (context == null || TextUtils.isEmpty(className))
+            return false;
+        ActivityManager am = (ActivityManager) context.getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningTaskInfo> list = am.getRunningTasks(1);
+//        boolean flag=false;
+        for (ActivityManager.RunningTaskInfo taskInfo : list) {
+            if (taskInfo.topActivity.getShortClassName().contains(className)) { // 说明它已经启动了
+//                flag = true;
+                return true;
+            }
+        }
+        return false;
     }
 }
